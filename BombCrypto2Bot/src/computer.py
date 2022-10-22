60dcf1e94a0df1ddd5bd0f8de97a7b61c2075166
Pablo Biajoni <pbiajoni@hotmail.com>
2022-10-22 09:50:33 -0300

    Rebooting computer

--- BombCrypto2Bot/src/computer.py
+++ BombCrypto2Bot/src/computer.py
@@ -1,9 +1,24 @@
+﻿from argparse import Action
 import platform
+import os
+
 class Computer:
+    def __init__(self):
+        from src.log import Log
+        from src.actions import Actions
+        self.log = Log()
+        self.actions = Actions()
+
     def reboot(self):
-        if platform.system() == "Windows": 
+        if platform.system() == "Windows":
+            self.log.console('Rebooting Windows',
+                         services=True, emoji='📄')
+            self.actions.sleep(5, 5, forceTime=True)
             os.system("shutdown.exe -r -t 0")
         else:
+            self.log.console('Rebooting Linux',
+                         services=True, emoji='📄')
+            self.actions.sleep(5, 5, forceTime=True)
             os.system("reboot")
 
 
